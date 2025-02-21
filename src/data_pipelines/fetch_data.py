592fd24e4d0f71a5fd87da4d6829f911a44ae049
cabersdev <caberlotto.giovanni@gmail.com>
2025-02-21 15:11:54 +0100

    init data_piplines

--- src/data_pipelines/fetch_data.py
+++ src/data_pipelines/fetch_data.py
@@ -9,3 +9,25 @@ RAW_DATA_DIR = Path("data/raw")
 PROCESSED_DATA_DIR = Path("data/processed")
 os.makedirs(RAW_DATA_DIR, exist_ok=True)
 os.makedirs(RAW_DATA_DIR, exist_ok=True)
+
+def fetch_and_process_stock_data(ticker: str, years: int = 10) -> None:
+    raw_data = yfinance.download(
+        tickers = ticker,
+        period=f"{years}y",
+        interval="1d",
+        progress=False
+    )
+
+    df_raw = pl.DataFrame(raw_data.reset_index())
+    df_raw.write_csv(RAW_DATA_DIR / f"{ticker}_raw.csv")
+
+    df_processed = df_raw.with_columns(
+        pl.col("Date").str.strptime(pl.Date, fmt="%Y-%m-%d"),
+        pl.col("Close").log().diff().alias("log_returns")
+    ).drop_nulls()
+
+    df_processed.write_csv(PROCESSED_DATA_DIR / f"{ticker}_processed.csv")
+
+    
+
+
